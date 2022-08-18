70622a2f5368adeb6d6a554952a143e094561734
Julio César G. Conceição <juliocesar.conceicao@novahaus.com.br>
2022-08-18 16:13:38 -0300

    fix: removed id of CreateUserRequest

--- APIEstudos/Commands/CreateUserRequest.cs
+++ APIEstudos/Commands/CreateUserRequest.cs
@@ -5,7 +5,6 @@ namespace APIEstudos.Requests
 {
     public class CreateUserRequest : IRequest<UserResponse>
     {
-        public Guid Id { get; set; }
         public string? Name { get; set; }
         public string? Email { get; set; }
         public DateTime Date { get; set; }
