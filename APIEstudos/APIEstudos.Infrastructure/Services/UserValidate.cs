5c8a0dc474285a9c743d7c525b3d9f67033ce052
Joaoms98 <jvm1800@gmail.com>
2022-09-13 00:32:23 -0300

    feat: Add  documentation

--- APIEstudos/APIEstudos.Infrastructure/Services/UserValidate.cs
+++ APIEstudos/APIEstudos.Infrastructure/Services/UserValidate.cs
@@ -4,6 +4,12 @@ namespace APIEstudos.Infrastructure.Services
 {
     public class UserValidate : IUserValidate
     {
+        /// <summary>
+        /// Method for validated if name and email is null, and if email contains caracters required
+        /// </summary>
+        /// <param name="name">Name of the user</param>
+        /// <param name="email">Email of the user</param>
+        /// <returns>true or exception message</returns>
         public bool UserIsValid(string name, string email)
         {
             if(email.Contains("@") && email.Contains(".com"))
